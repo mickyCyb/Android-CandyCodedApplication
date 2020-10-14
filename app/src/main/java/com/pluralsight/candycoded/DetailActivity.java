ca3820bee09115e5f5b5f7409c075e92dc3203f9
mickyCyb <michaelgashaw2009@gmail.com>
2020-10-14 20:44:26 +0300

    share a candy with an intent completed

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -24,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        createShareIntent();
         return super.onOptionsItemSelected(item);
     }
 
